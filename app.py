96662d1638f4b6757dd8276fc6162964dec318f1
Nick <info@nlakersloot.nl>
2025-06-18 13:37:48 +0200

    Show themes inline before calendar

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import streamlit as st
 
 st.title("Kalender en Afbeeldingen")
 
+# Eerst de drie hoofdthema's naast elkaar
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write("## Gezin")
+    st.image("https://loremflickr.com/400/300/family", caption="Gezin")
+
+with col2:
+    st.write("## Moeder")
+    st.image("https://loremflickr.com/400/300/mother", caption="Moeder")
+
+with col3:
+    st.write("## Lover")
+    st.image("https://loremflickr.com/400/300/lovers", caption="Lover")
+
+# Daaronder de kalender
 st.write("Selecteer een datum:")
 selected_date = st.date_input("Datum")
-
-st.write("## Gezin")
-st.image("https://loremflickr.com/400/300/family", caption="Gezin")
-
-st.write("## Moeder")
-st.image("https://loremflickr.com/400/300/mother", caption="Moeder")
-
-st.write("## Lover")
-st.image("https://loremflickr.com/400/300/lovers", caption="Lover")
